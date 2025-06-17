b4d4cefad1c9035f637ca539b3eb7061635e8baf
GlebPronin <s2akk737@gmail.com>
2025-06-17 15:21:20 +0300

    Add files via upload

---
+++ OrderService/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text;
+
+[ApiController]
+[Route("[controller]")]
+public class OrderController : ControllerBase
+{
+    private readonly IProducer<Null, string> _producer;
+
+    public OrderController(IProducer<Null, string> producer)
+    {
+        _producer = producer;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateOrder()
+    {
+        var order = new { OrderId = Guid.NewGuid(), Timestamp = DateTime.UtcNow };
+        var json = JsonSerializer.Serialize(order);
+
+        await _producer.ProduceAsync("orders", new Message<Null, string> { Value = json });
+
+        return Ok(new { message = "Order sent to Kafka", order });
+    }
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok("Order service is running");
+    }
+}
---
+++ OrderService/Program.cs
@@ -0,0 +1,14 @@
+using Confluent.Kafka;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton(new ProducerBuilder<Null, string>(
+    new ProducerConfig { BootstrapServers = "kafka:9092" }).Build());
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
